d6d71f055e7819a4c9c7505b4382416ca8af4eb9
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2020-09-11 10:15:26 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package main.java.AdamsGroupID.AdamsArtifactID;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -20,70 +21,71 @@ import com.google.gson.JsonObject;
  */
 public class Main {
 
-   
-    public static void main(String[] args) throws IOException {
-        
-    	//Create a HashMap to store the student data. The key is their ID #, the value is a Student object
-        HashMap<Integer, Student> myStudents = XLSXReader.importStudentData();
-        
-        //Setting the original test scores for each student
-        XLSXReader.importTestScores(myStudents, true, "data\\Test Scores.xlsx");
-        
-        //Setting the re-take scores for students who took it
-        XLSXReader.importTestScores(myStudents, false, "data\\Test Retake Scores.xlsx");
-        
-        //Get the test average saved to testAverage
-        double numberOfTests = myStudents.size();
-        double testTotal = 0; 
-        for (Student i : myStudents.values()) {
-          testTotal += i.getFinalScore();
-        }
-        double testAverage = testTotal / numberOfTests; 
-        
-        //Create an Array for all the female students majoring in computer science
-        ArrayList<String> femaleCS = new ArrayList<String>();
-        //Add in all females in CS
-        for (Student i : myStudents.values()) {
-            if(i.getMajor().equals("computer science") && i.getGender() == 'F'){
-        	femaleCS.add(i.getId());
-            }
-        }
-        //Put in order based on ID
-        Collections.sort(femaleCS);
-        
-        //Sends a POST request to "/challenge" with the JSON data specified in the coding challenge instructions
-        postRequest("adambadagliacco@gmail.com", "Adam Badagliacco", (int)testAverage, femaleCS);
-    }
-    
-    public static void postRequest(String id, String name, int average, ArrayList<String> studentIds) throws IOException{
-    	
-    	// Create new JSON Object containing our data to POST
-    	JsonObject myJSON = new JsonObject();
-    	myJSON.addProperty("id", id);
-    	myJSON.addProperty("name", name);
-    	myJSON.addProperty("average", average);
-    	//Making the ArrayList to a string so it can be added to myJSON
-    	Gson gsonBuilder = new GsonBuilder().create();
-    	String jsonFromJavaArrayList = gsonBuilder.toJson(studentIds);
-    	myJSON.addProperty("studentIds", jsonFromJavaArrayList);
-        
-        //POST myJSON to "/challenge"
-        URL url = new URL("/challenge");
-        URLConnection con = url.openConnection();
-        HttpURLConnection http = (HttpURLConnection)con;
-        http.setRequestMethod("POST"); 
-        http.setDoOutput(true);
-        
-        byte[] out = myJSON.toString().getBytes(StandardCharsets.UTF_8);
-        int length = out.length;
+	public static void main(String[] args) throws IOException {
 
-        http.setFixedLengthStreamingMode(length);
-        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        http.connect();
-        try(OutputStream os = http.getOutputStream()) {
-            os.write(out);
-        }
-       
-        
-    }
+		// Create a HashMap to store the student data. The key is their ID #, the value
+		// is a Student object
+		HashMap<Integer, Student> myStudents = XLSXReader.importStudentData();
+
+		// Setting the original test scores for each student
+		XLSXReader.importTestScores(myStudents, true, "data\\Test Scores.xlsx");
+
+		// Setting the re-take scores for students who took it
+		XLSXReader.importTestScores(myStudents, false, "data\\Test Retake Scores.xlsx");
+
+		// Get the test average saved to testAverage
+		double numberOfTests = myStudents.size();
+		double testTotal = 0;
+		for (Student i : myStudents.values()) {
+			testTotal += i.getFinalScore();
+		}
+		double testAverage = testTotal / numberOfTests;
+
+		// Create an Array for all the female students majoring in computer science
+		ArrayList<String> femaleCS = new ArrayList<String>();
+		// Add in all females in CS
+		for (Student i : myStudents.values()) {
+			if (i.getMajor().equals("computer science") && i.getGender() == 'F') {
+				femaleCS.add(i.getId());
+			}
+		}
+		// Put in order based on ID
+		Collections.sort(femaleCS);
+
+		// Sends a POST request to "/challenge" with the JSON data specified in the
+		// coding challenge instructions
+		postRequest("adambadagliacco@gmail.com", "Adam Badagliacco", (int) testAverage, femaleCS);
+	}
+
+	public static void postRequest(String id, String name, int average, ArrayList<String> studentIds)
+			throws IOException {
+
+		// Create new JSON Object containing our data to POST
+		JsonObject myJSON = new JsonObject();
+		myJSON.addProperty("id", id);
+		myJSON.addProperty("name", name);
+		myJSON.addProperty("average", average);
+		// Making the ArrayList to a string so it can be added to myJSON
+		Gson gsonBuilder = new GsonBuilder().create();
+		String jsonFromJavaArrayList = gsonBuilder.toJson(studentIds);
+		myJSON.addProperty("studentIds", jsonFromJavaArrayList);
+
+		// POST myJSON to "/challenge"
+		URL url = new URL("/challenge");
+		URLConnection con = url.openConnection();
+		HttpURLConnection http = (HttpURLConnection) con;
+		http.setRequestMethod("POST");
+		http.setDoOutput(true);
+
+		byte[] out = myJSON.toString().getBytes(StandardCharsets.UTF_8);
+		int length = out.length;
+
+		http.setFixedLengthStreamingMode(length);
+		http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		http.connect();
+		try (OutputStream os = http.getOutputStream()) {
+			os.write(out);
+		}
+
+	}
 }
--- XLSXReader.java
+++ XLSXReader.java
@@ -12,74 +12,74 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XLSXReader {
-	
+
 	private static XSSFWorkbook wb;
 
 	/*
 	 * Returns a HashMap containing Student data read in from Excel
 	 */
-	public static HashMap<Integer, Student> importStudentData() throws IOException{
-		
+	public static HashMap<Integer, Student> importStudentData() throws IOException {
+
 		HashMap<Integer, Student> toBeReturned = new HashMap<Integer, Student>();
-		
-        wb = new XSSFWorkbook(new FileInputStream(new File("data\\Student Info.xlsx")));
-        XSSFSheet sheet = wb.getSheetAt(0); 
-        Iterator<Row> itr = sheet.iterator(); // Used to iterate through the rows and cells
-        
-        itr.next(); //Skip the heading row
-        
-        while (itr.hasNext()) {//Continues to iterate through each row until no more data is present in excel
+
+		wb = new XSSFWorkbook(new FileInputStream(new File("data\\Student Info.xlsx")));
+		XSSFSheet sheet = wb.getSheetAt(0);
+		Iterator<Row> itr = sheet.iterator(); // Used to iterate through the rows and cells
+
+		itr.next(); // Skip the heading row
+
+		while (itr.hasNext()) {// Continues to iterate through each row until no more data is present in excel
 			Row row = itr.next();
-			Iterator<Cell> cellIterator = row.cellIterator(); 
-																
-			while (cellIterator.hasNext()) { //Creates a new student in our array out of the data in each row
+			Iterator<Cell> cellIterator = row.cellIterator();
+
+			while (cellIterator.hasNext()) { // Creates a new student in our array out of the data in each row
 				Cell cell = cellIterator.next();
-				int id = (int)cell.getNumericCellValue();
-				
+				int id = (int) cell.getNumericCellValue();
+
 				cell = cellIterator.next();
-	            String major = cell.getStringCellValue();
-	            
-	            cell = cellIterator.next();
-	            String gender = cell.getStringCellValue();
-	            
-	            toBeReturned.put(id, new Student(Integer.toString(id), major, gender.charAt(0)));
+				String major = cell.getStringCellValue();
+
+				cell = cellIterator.next();
+				String gender = cell.getStringCellValue();
+
+				toBeReturned.put(id, new Student(Integer.toString(id), major, gender.charAt(0)));
 			}
-        }
-        return toBeReturned;
-    }
-	
+		}
+		return toBeReturned;
+	}
+
 	/*
 	 * Updates each students Test Score with the data from the Excel Sheet
 	 * 
 	 * testType is true for the original test and false for the reTake
 	 */
-	public static void importTestScores(HashMap<Integer, Student> myStudents, boolean testType, String filePath) throws FileNotFoundException, IOException{
-		
+	public static void importTestScores(HashMap<Integer, Student> myStudents, boolean testType, String filePath)
+			throws FileNotFoundException, IOException {
+
 		wb = new XSSFWorkbook(new FileInputStream(new File(filePath)));
-        XSSFSheet sheet = wb.getSheetAt(0); 
-        Iterator<Row> itr = sheet.iterator(); // Used to iterate through the rows and cells
-        
-        itr.next(); //Skip the heading row
-        
-        while (itr.hasNext()) {//Continues to iterate through each row until no more data is present in excel
+		XSSFSheet sheet = wb.getSheetAt(0);
+		Iterator<Row> itr = sheet.iterator(); // Used to iterate through the rows and cells
+
+		itr.next(); // Skip the heading row
+
+		while (itr.hasNext()) {// Continues to iterate through each row until no more data is present in excel
 			Row row = itr.next();
-			Iterator<Cell> cellIterator = row.cellIterator(); 
-																
-			while (cellIterator.hasNext()) { //Creates a new student in our array out of the data in each row
+			Iterator<Cell> cellIterator = row.cellIterator();
+
+			while (cellIterator.hasNext()) { // Creates a new student in our array out of the data in each row
 				Cell cell = cellIterator.next();
-				int id = (int)cell.getNumericCellValue();
-				
+				int id = (int) cell.getNumericCellValue();
+
 				cell = cellIterator.next();
-	            double score = cell.getNumericCellValue();
-	            
-	            if(testType){
-	            	myStudents.get(id).setTestScore(score);
-	            }
-	            else{
-	            	myStudents.get(id).setReTakeScore(score);
-	            }
+				double score = cell.getNumericCellValue();
+
+				if (testType) {
+					myStudents.get(id).setTestScore(score);
+				} else {
+					myStudents.get(id).setReTakeScore(score);
+				}
 			}
-        }
-		
+		}
+
 	}
 }
