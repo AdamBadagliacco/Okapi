9cdd527f430d1140c778578ce510aaba1c2ab27d
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2020-09-11 09:46:50 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+package main.java.AdamsGroupID.AdamsArtifactID;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+/**
+ * Made to fulfill the requirements specified in the Coding Challenge
+ * 
+ * @author Adam Badagliacco
+ * @Date 9/11/2020
+ */
+public class Main {
+
+   
+    public static void main(String[] args) throws IOException {
+        
+    	//Create a HashMap to store the student data. The key is their ID #, the value is a Student object
+        HashMap<Integer, Student> myStudents = XLSXReader.importStudentData();
+        
+        //Setting the original test scores for each student
+        XLSXReader.importTestScores(myStudents, true, "data\\Test Scores.xlsx");
+        
+        //Setting the re-take scores for students who took it
+        XLSXReader.importTestScores(myStudents, false, "data\\Test Retake Scores.xlsx");
+        
+        //Get the test average saved to testAverage
+        double numberOfTests = myStudents.size();
+        double testTotal = 0; 
+        for (Student i : myStudents.values()) {
+          testTotal += i.getFinalScore();
+        }
+        double testAverage = testTotal / numberOfTests; 
+        
+        //Create an Array for all the female students majoring in computer science
+        ArrayList<String> femaleCS = new ArrayList<String>();
+        //Add in all females in CS
+        for (Student i : myStudents.values()) {
+            if(i.getMajor().equals("computer science") && i.getGender() == 'F'){
+        	femaleCS.add(i.getId());
+            }
+        }
+        //Put in order based on ID
+        Collections.sort(femaleCS);
+        
+        //Sends a POST request to "/challenge" with the JSON data specified in the coding challenge instructions
+        postRequest("adambadagliacco@gmail.com", "Adam Badagliacco", (int)testAverage, femaleCS);
+    }
+    
+    public static void postRequest(String id, String name, int average, ArrayList<String> studentIds) throws IOException{
+    	
+    	// Create new JSON Object containing our data to POST
+    	JsonObject myJSON = new JsonObject();
+    	myJSON.addProperty("id", id);
+    	myJSON.addProperty("name", name);
+    	myJSON.addProperty("average", average);
+    	//Making the ArrayList to a string so it can be added to myJSON
+    	Gson gsonBuilder = new GsonBuilder().create();
+    	String jsonFromJavaArrayList = gsonBuilder.toJson(studentIds);
+    	myJSON.addProperty("studentIds", jsonFromJavaArrayList);
+        
+        //POST myJSON to "/challenge"
+        URL url = new URL("/challenge");
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); 
+        http.setDoOutput(true);
+        
+        byte[] out = myJSON.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+       
+        
+    }
+}
---
+++ Student.java
@@ -0,0 +1,97 @@
+package main.java.AdamsGroupID.AdamsArtifactID;
+
+public class Student {
+	
+    private String id;
+    private String major;
+    private char gender; //Typically 'M' or 'F' for male or female
+    
+    private double testScore;
+    private double reTakeScore;
+    
+    public Student(String id, String major, char gender){
+        this.id = id;
+        this.major = major;
+        this.gender = gender; 
+        
+        //Set the test scores to 0 so they aren't null; They will be assigned through the setter methods
+        testScore = 0;
+        reTakeScore = 0;
+    }
+    
+    public String toString(){
+    	return id + " " + major + " " + gender + " " + testScore + " " + reTakeScore + " ";
+    }
+    
+    public double getFinalScore(){
+    	if(testScore > reTakeScore){
+    		return testScore;
+    	}
+    	else{
+    		return reTakeScore;
+    	}
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the major
+     */
+    public String getMajor() {
+        return major;
+    }
+
+    /**
+     * @param major the major to set
+     */
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    /**
+     * @return the gender
+     */
+    public char getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+
+	public double getTestScore() {
+		return testScore;
+	}
+
+
+	public void setTestScore(double testScore) {
+		this.testScore = testScore;
+	}
+
+
+	public double getReTakeScore() {
+		return reTakeScore;
+	}
+
+
+	public void setReTakeScore(double reTakeScore) {
+		this.reTakeScore = reTakeScore;
+	}
+    
+}
